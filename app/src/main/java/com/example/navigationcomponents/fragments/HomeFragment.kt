d140ac9108764c1c1abb3c14a38542248fb5c77c
Rohitohlyan66 <ohlyanrohit66@gmail.com>
2021-06-03 21:44:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigationcomponents/fragments/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.navigationcomponents.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.navigationcomponents.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_login.setOnClickListener {
+
+            val action = HomeFragmentDirections.actionHomeFragment2ToLoginFragment()
+            findNavController().navigate(action)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/navigationcomponents/fragments/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.navigationcomponents.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.navigationcomponents.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
